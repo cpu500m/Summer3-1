cbf1a4d57a9f7f6d0f73424ce39231c5e1d3fe3e
paulkim <paul108203@naver.com>
2022-09-19 18:19:55 +0900

    2022-09-18 B2_1731

---
+++ src/B2_1731.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class B2_1731 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr =new int[N];
+        for(int i = 0 ; i < N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+        if(arr[2]-arr[1] == arr[1]-arr[0])
+            bw.write(String.valueOf(arr[N-1]+(arr[2]-arr[1])));
+        else
+            bw.write(String.valueOf(arr[N-1]*(arr[2]/arr[1])));
+        bw.flush();
+    }
+}
