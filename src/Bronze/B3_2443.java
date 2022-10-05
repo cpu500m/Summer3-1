3df08da5a70513457d081fce39daefbd4887ad27
paulkim <paul108203@naver.com>
2022-10-05 16:57:50 +0900

    2022-10-05 B3_2443

---
+++ src/Bronze/B3_2443.java
@@ -0,0 +1,28 @@
+package Bronze;
+
+import java.io.*;
+
+public class B3_2443 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        char[][] arr = new char[N+1][2*N];
+        for(int i = N; i  > 0; i--){
+            for(int j = 1; j < N ; j++)
+                arr[i][j]= ' ';
+            for(int j = N ; j >N-i ; j--)
+                arr[i][j] = '*';
+            for(int j = N; j < N+i ; j++)
+                arr[i][j] = '*';
+        }
+        for(int i = N ; i  > 0; i--){
+            for(int j = 1 ; j < N+i ; j++)
+                bw.write(arr[i][j]);
+            if( i !=1 )
+                bw.write("\n");
+        }
+        bw.flush();
+    }
+}
