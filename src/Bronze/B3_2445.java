98577a7c6c6f2cb6db2e6a43b8c65399e581e074
paulkim <paul108203@naver.com>
2022-10-08 13:37:27 +0900

    2022-10-08 B3_2445

--- src/Bronze/B3_2445.java
+++ src/Bronze/B3_2445.java
@@ -9,24 +9,24 @@ public class B3_2445 {
 
         int N = Integer.parseInt(br.readLine());
 
-        char[][] arr = new char[N+1][2*N+1];
-        for(int i = 1; i < N+1; i++){
-            for(int j = 1; j < 2*N+1 ; j++)
+        char[][] arr = new char[N+1][2*N];
+        for(int i = N; i  > 0; i--){
+            for(int j = 1; j < N ; j++)
                 arr[i][j]= ' ';
-            for(int j = 1 ; j < 1+i ; j++)
+            for(int j = N ; j >N-i ; j--)
                 arr[i][j] = '*';
-            for(int j = 2*N; j > 2*N-i ; j--)
+            for(int j = N; j < N+i ; j++)
                 arr[i][j] = '*';
         }
-        for(int i = 1 ; i <N+1; i++){
-            for(int j = 1 ; j < 2*N+1 ; j++)
+        for(int i = N ; i  > 0; i--){
+            for(int j = 1 ; j < N+i ; j++)
                 bw.write(arr[i][j]);
             bw.write("\n");
         }
-        for(int i = N-1 ; i >0; i--){
-            for(int j = 1 ; j < 2*N+1 ; j++)
+        for(int i = 2 ; i  < N+1; i++){
+            for(int j = 1 ; j < N+i ; j++)
                 bw.write(arr[i][j]);
-            if( i !=1 )
+            if( i !=N )
                 bw.write("\n");
         }
         bw.flush();
