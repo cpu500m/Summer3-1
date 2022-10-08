251fd7d7dbeb5fcb5452c8da732dcd8cd3294587
paulkim <paul108203@naver.com>
2022-10-08 13:37:53 +0900

    2022-10-08 B3_2446

--- src/Bronze/B3_2446.java
+++ src/Bronze/B3_2446.java
@@ -2,7 +2,7 @@ package Bronze;
 
 import java.io.*;
 
-public class B3_2445 {
+public class B3_2446 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
