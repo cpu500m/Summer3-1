3458996d60fc066a1197f0a4e8c0d73bd2097620
paulkim <paul108203@naver.com>
2022-10-30 14:03:05 +0900

    2022-10-30 B3_2455

---
+++ src/Bronze/B3_2455.java
@@ -0,0 +1,23 @@
+package Bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B3_2455 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int cur = 0;
+        int max = 0;
+        for(int i = 0 ;i < 4 ; i++){
+            st = new StringTokenizer(br.readLine());
+            cur -= Integer.parseInt(st.nextToken());
+            cur += Integer.parseInt(st.nextToken());
+            if( cur > max)
+                max = cur;
+        }
+        bw.write(String.valueOf(max));
+        bw.flush();
+    }
+}
