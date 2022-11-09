3f0379195bdc29ca449d17420cf6f535d80c44f8
paulkim <paul108203@naver.com>
2022-11-09 16:56:52 +0900

    2022-11-09 B5_5338

---
+++ src/Bronze/B5_5338.java
@@ -0,0 +1,16 @@
+package Bronze;
+
+import java.io.*;
+
+public class B5_5338 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("       _.-;;-._\n");
+        bw.write("'-..-'|   ||   |\n");
+        bw.write("'-..-'|_.-;;-._|\n");
+        bw.write("'-..-'|   ||   |\n");
+        bw.write("'-..-'|_.-''-._|\n");
+        bw.flush();
+    }
+}
