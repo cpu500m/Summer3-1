28fd563b4bca8770730eed6a744357a729ebad48
paulkim <paul108203@naver.com>
2022-11-10 12:59:29 +0900

    2022-11-10 B5_5337

---
+++ src/Bronze/B5_5337.java
@@ -0,0 +1,14 @@
+package Bronze;
+
+import java.io.*;
+
+public class B5_5337 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(".  .   .\n");
+        bw.write("|  | _ | _. _ ._ _  _\n");
+        bw.write("|/\\|(/.|(_.(_)[ | )(/.\n");
+
+        bw.flush();
+    }
+}
