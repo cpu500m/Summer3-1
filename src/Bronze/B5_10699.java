f6c2ca9d5ca2cd99ef1f7055e7d797be7e967b29
cpu500m <paul108203@naver.com>
2023-01-29 21:45:59 +0900

    2023-01-29

---
+++ src/Bronze/B5_10699.java
@@ -0,0 +1,10 @@
+package Bronze;
+
+import java.time.LocalDate;
+
+public class B5_10699 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
