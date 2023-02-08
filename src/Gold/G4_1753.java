709d9be98238b34d02202f692ee183290fad2047
cpu500m <paul108203@naver.com>
2023-02-08 18:47:10 +0900

    2023-02-07 G4_1753 swea 문제 집중,..

--- src/Gold/G4_1753.java
+++ src/Gold/G4_1753.java
@@ -1,10 +1,9 @@
-// 이번에는 우선순위 큐로 한번 맛깔나게
 package Gold;
 
 import java.io.*;
 import java.util.*;
 
-public class G5_1916_2 {
+public class G4_1753 {
     static class City{
         int distance , city_idx;
         City(int distance, int city_idx){
@@ -12,38 +11,53 @@ public class G5_1916_2 {
             this.city_idx = city_idx;
         }
     }
+    static class Node{
+        int dest , distance;
+        Node(int dest, int distance){
+            this.dest =dest;
+            this.distance = distance;
+        }
+    }
     final static int MAX = 1_000_000_001;
+    static ArrayList<Node>[] arrayList;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        // 걍 매번 -1 -1 하기 귀찮아서 한행 한열 더뒀음 어차피 넉넉하니깐
-        int[][] graph = new int[N+1][N+1];
+        StringBuilder sb =new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int start = Integer.parseInt(br.readLine());
+
+        arrayList = new ArrayList[N+1];
+        for(int i = 0 ; i <= N ; i++)
+            arrayList[i] = new ArrayList<>();
+
         int[] distance = new int[N+1];
         Arrays.fill(distance,MAX);
-        for(int i = 0 ; i < N+1 ; i++)
-            Arrays.fill(graph[i] ,MAX);
-        StringTokenizer st;
         for(int i = 0 ; i < M ; i++){
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            if(graph[s][e] > cost)
-                graph[s][e] = cost;
+            arrayList[s].add(new Node(e,cost));
         }
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int destination = Integer.parseInt(st.nextToken());
 
         // 다익스트라
-        dijkstra(start, distance, graph, N , destination);
-        bw.write(String.valueOf(distance[destination]));
+        dijkstra(start, distance, N );
+        for(int i = 1 ; i <= N ; i++) {
+            if(distance[i] == MAX)
+                sb.append("INF\n");
+            else
+                sb.append(distance[i]+"\n");
+        }
+        bw.write(sb.toString());
         bw.flush();
     }
-    static void dijkstra(int start,int[] distance, int[][] graph, int N , int destination){
+    static void dijkstra(int start,int[] distance,  int N){
         PriorityQueue<City> p_queue = new PriorityQueue<>(new Comparator<City>() {
             @Override
             public int compare(City o1, City o2) {
@@ -58,11 +72,11 @@ public class G5_1916_2 {
             int cur_city = cur.city_idx;
             int cur_distance = cur.distance;
             visit[cur_city] = true;
-            for(int i= 1 ; i <= N ;i++){
-                if(visit[i]) continue;
-                if(distance[i] > cur_distance + graph[cur_city][i]) {
-                    distance[i] = cur_distance+graph[cur_city][i];
-                    p_queue.add(new City(distance[i], i));
+            for(Node n : arrayList[cur_city]){
+                if(visit[n.dest]) continue;
+                if(distance[n.dest] > cur_distance + n.distance) {
+                    distance[n.dest] = cur_distance+n.distance;
+                    p_queue.add(new City(distance[n.dest], n.dest));
                 }
             }
         }
