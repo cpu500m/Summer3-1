8a8805a32883f7008258822d783f126f1c49f5fd
cpu500m <paul108203@naver.com>
2023-02-26 00:46:56 +0900

    2023-02-25 P4_10999,G1_1275,G1_2268,B5_20492 삼성 알고리즘 과정 끝!!

---
+++ src/Gold/G1_2268.java
@@ -0,0 +1,75 @@
+package Gold;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class G1_2268 {
+    static int N;
+
+    static long[] seg_tree;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        // 세그먼트 트리 생성
+        seg_tree = new long[N << 2];
+
+        /// 업데이트 또는 쿼리 처리
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int act = Integer.parseInt(st.nextToken());
+            if(act == 0) {
+                int l = Integer.parseInt(st.nextToken());
+                int r = Integer.parseInt(st.nextToken());
+                if( r < l){
+                    int temp = l;
+                    l = r;
+                    r = temp;
+                }
+                long val = query(1, 1, N, l, r);
+                sb.append(val + "\n");
+            }
+            else {
+                int idx = Integer.parseInt(st.nextToken());
+                long val = Long.parseLong(st.nextToken());
+                update(1, 1, N, idx, val);
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+
+
+    static void update(int n, int s, int e, int i, long v) {
+
+        if (e < i || i < s) return;
+        if (s == e) {
+            seg_tree[n] = v;
+            return;
+        }
+
+        int mid = (s + e) >> 1;
+        update(n * 2, s, mid, i,v);
+        update(n * 2 | 1, mid + 1, e, i, v);
+        seg_tree[n] = seg_tree[n * 2] + seg_tree[n * 2 | 1];
+    }
+
+    static long query(int n, int s, int e, int i, int j) {
+
+        if (e < i || j < s) return 0;
+        if (i <= s && e <= j) {
+            return seg_tree[n];
+        }
+
+        int mid = (s + e) >> 1;
+        long l = query(n * 2, s, mid, i, j);
+        long r = query(n * 2 | 1, mid + 1, e, i, j);
+        return l + r;
+    }
+}
