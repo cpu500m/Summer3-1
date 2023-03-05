e6a08ad178cf916fda13f4eb86991aa39eb6a188
cpu500m <paul108203@naver.com>
2023-03-05 21:15:30 +0900

    2023-03-05 G3_2143

--- src/Gold/G3_2143.java
+++ src/Gold/G3_2143.java
@@ -8,6 +8,7 @@
 package Gold;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class G3_2143 {
@@ -16,23 +17,85 @@ public class G3_2143 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         /*      입력  처리      */
-        // index
-        int i,j;
 
         int T = Integer.parseInt(br.readLine());
         int n =  Integer.parseInt(br.readLine());
         int[] A = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(i = 0 ; i < n; i++)
+        for(int i = 0 ; i < n; i++)
             A[i] =  Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(br.readLine());
         int[] B = new int[m];
         st = new StringTokenizer(br.readLine());
-        for(i = 0 ; i < m ; i++)
+        for(int i = 0 ; i < m ; i++)
             B[i] = Integer.parseInt(st.nextToken());
 
-        /*    로직    */
+        // 1. 일단 모든 가짓수를 포함하는 배열을 생성한다.
+        int a_size , b_size;
+        if( n % 2 == 0)
+            a_size = (n+1)*(n/2);
+        else a_size = (n+1)*(n/2)+(n+1)/2;
 
+        if(m %2 == 0)
+            b_size = (m+1)*(m/2);
+        else b_size = (m+1)*(m/2)+(m+1)/2;
+        int[] a_total = new int[a_size];
+        int[] b_total = new int[b_size];
 
+        int cur_idx = 0;
+        int sum;
+        // a 부분
+        for(int i =0 ; i < n ; i++){
+            // 누적 값
+            sum = 0;
+            for(int j = i ; j < n ; j++){
+                sum += A[j];
+                a_total[cur_idx++] = sum;
+            }
+        }
+        // b 부분
+        cur_idx = 0;
+        for(int i =0 ; i < m ; i++){
+            // 누적 값
+            sum = 0;
+            for(int j = i ; j < m ; j++){
+                sum += B[j];
+                b_total[cur_idx++] = sum;
+            }
+        }
+
+        // 이제.. 이분탐색을 하면됨. 그전에 정렬
+        Arrays.sort(b_total);
+        long res = 0;
+        for(int i = 0 ; i< a_size ; i++){
+            int target = T - a_total[i];
+            int min_idx = get_min_idx(target,b_total);
+            int max_idx = get_max_idx(target,b_total);
+            res += (max_idx - min_idx);
+        }
+        bw.write(String.valueOf(res));
+        bw.flush();
+    }
+    static int get_min_idx(int target,int[] arr){
+        int left = 0 ;
+        int right = arr.length-1;
+        while (left <= right){
+            int mid = (left+right)>>1;
+            if(arr[mid] >= target)
+                right = mid-1;
+            else left = mid+1;
+        }
+        return right;
+    }
+    static int get_max_idx(int target, int[] arr){
+        int left = 0 ;
+        int right = arr.length-1;
+        while (left <= right){
+            int mid = (left+right)>>1;
+            if(arr[mid] <= target)
+                left = mid+1;
+            else right = mid-1;
+        }
+        return right;
     }
 }
