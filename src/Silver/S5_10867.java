ee21394a4117c6390dad2be1832e07cf9510674f
cpu500m <paul108203@naver.com>
2023-04-14 19:34:16 +0900

    2023-04-14 S5_10687

---
+++ src/Silver/S5_10867.java
@@ -0,0 +1,26 @@
+package Silver;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class S5_10867 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < n ; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        sb.append(arr[0]+" ");
+        for(int i = 1 ; i < n ; i++){
+            if(arr[i] != arr[i-1])
+                sb.append(arr[i]+" ");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
