dc4a9c2d2b4bb6d5bc53a82c61628f2a8704f6ef
cpu500m <paul108203@naver.com>
2023-04-19 14:33:54 +0900

    2023-04-19 B3_11719

---
+++ src/Bronze/B3_11719.java
@@ -0,0 +1,17 @@
+package Bronze;
+
+import java.io.*;
+
+public class B3_11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String str;
+        while ((str = br.readLine()) != null){
+            sb.append(str+"\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
