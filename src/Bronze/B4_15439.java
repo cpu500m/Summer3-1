7bc312ba4eab12dbf624be2b23b73e6d561e63c2
cpu500m <paul108203@naver.com>
2023-05-30 20:20:51 +0900

    2023-05-30 B4_15139

---
+++ src/Bronze/B4_15439.java
@@ -0,0 +1,13 @@
+package Bronze;
+
+import java.io.*;
+
+public class B4_15439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(n*(n-1)));
+        bw.flush();
+    }
+}
