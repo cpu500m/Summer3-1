9cc02d287272c6c1785269a1ba48a368e99bec7c
cpu500m <paul108203@naver.com>
2023-05-31 22:53:49 +0900

    2023-05-31 B4_15000

---
+++ src/Bronze/B4_15000.java
@@ -0,0 +1,12 @@
+package Bronze;
+
+import java.io.*;
+
+public class B4_15000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(br.readLine().toUpperCase());
+        bw.flush();
+    }
+}
