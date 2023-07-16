70308c4a821f86f4f103d1efb00371c3e4ea9f53
cpu500m <paul108203@naver.com>
2023-07-16 21:42:35 +0900

    2023-07-16 B3_2914

---
+++ src/Bronze/B3_2914.java
@@ -0,0 +1,10 @@
+package Bronze;
+
+import java.util.Scanner;
+
+public class B3_2914 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(sc.nextInt()*(sc.nextInt()-1)+1);
+    }
+}
