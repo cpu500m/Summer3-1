f8950fb75a78495e588a888c62a8000154760204
cpu500m <paul108203@naver.com>
2023-08-23 22:06:47 +0900

    2023-08-23 B5_15962

---
+++ src/Bronze/B5_15962.java
@@ -0,0 +1,12 @@
+package Bronze;
+
+import java.io.*;
+
+public class B5_15962 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("파이팅!!");
+        bw.flush();
+    }
+}
