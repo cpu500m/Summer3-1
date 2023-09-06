df91a7c19f4d130f0b71e7619fcc355accbac04f
cpu500m <paul108203@naver.com>
2023-09-06 22:28:37 +0900

    2023-09-06 B1_2748

---
+++ src/Bronze/B1_2748.java
@@ -0,0 +1,19 @@
+package Bronze;
+
+import java.io.*;
+
+public class B1_2748 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[91];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        bw.write(String.valueOf(arr[n]));
+        bw.flush();
+    }
+}
