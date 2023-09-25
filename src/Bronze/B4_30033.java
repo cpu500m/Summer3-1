7b57bf5f0e6afa91eeea21b05ab0324ccc7f048f
cpu500m <paul108203@naver.com>
2023-09-25 22:24:20 +0900

    2023-09-25 B4_30033

---
+++ src/Bronze/B4_30033.java
@@ -0,0 +1,25 @@
+package Bronze;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class B4_30033 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int[] plan = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            plan[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int res = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            if(Integer.parseInt(st.nextToken()) >= plan[i]) res++;
+        }
+        bw.write(String.valueOf(res));
+        bw.flush();
+    }
+}
