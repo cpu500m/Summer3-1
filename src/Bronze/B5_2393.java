dcedc19c6692a829d7bc5a8e85c6ad0dfbb515c3
cpu500m <paul108203@naver.com>
2023-10-26 22:24:46 +0900

    2023-10-26 B5_2393

---
+++ src/Bronze/B5_2393.java
@@ -0,0 +1,23 @@
+package Bronze;
+
+import java.io.*;
+
+public class B5_2393 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("  ___  ___  ___\n");
+        bw.write("  | |__| |__| |\n");
+        bw.write("  |           |\n");
+        bw.write("   \\_________/\n");
+        bw.write("    \\_______/\n");
+        bw.write("     |     |\n");
+        bw.write("     |     |\n");
+        bw.write("     |     |\n");
+        bw.write("     |     |\n");
+        bw.write("     |_____|\n");
+        bw.write("  __/       \\__\n");
+        bw.write(" /             \\\n");
+        bw.write("/_______________\\");
+        bw.flush();
+    }
+}
