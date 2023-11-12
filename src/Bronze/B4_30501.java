36435d10bff69d933db6787a3398b876103ce80e
cpu500m <paul108203@naver.com>
2023-11-12 22:59:39 +0900

    2023-11-12 B4_30501

---
+++ src/Bronze/B4_30501.java
@@ -0,0 +1,17 @@
+package Bronze;
+
+import java.io.*;
+
+public class B4_30501 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            if(str.contains("S"))
+                bw.write(str);
+        }
+        bw.flush();
+    }
+}
