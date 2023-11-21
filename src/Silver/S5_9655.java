47834e819d277feaa501a9fd0e61df64ceffcf3f
cpu500m <paul108203@naver.com>
2023-11-21 22:45:45 +0900

    2023-11-21 S5_9655

--- src/Silver/S5_9655.java
+++ src/Silver/S5_9655.java
@@ -9,8 +9,8 @@ public class S5_9655 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        if(n <=3 ) bw.write("SK");
-        else if(n % 2 != 0) bw.write("SK");
+
+       if(n % 2 != 0) bw.write("SK");
         else bw.write("CY");
         bw.flush();
     }
